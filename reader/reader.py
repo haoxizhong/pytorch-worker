4b7dc9db969a7bed413f29ad5cc92c9ef389d3c7
xiaocj <xiaocj16@mails.tsinghua.edu.cn>
2019-11-14 21:49:30 +0800

    fix a bug for mode error

--- reader/reader.py
+++ reader/reader.py
@@ -15,13 +15,13 @@ def init_formatter(config, task_list, *args, **params):
         formatter[task] = form.init_formatter(config, task, *args, **params)
 
         def train_collate_fn(data):
-            return formatter["train"].process(data, config, task)
+            return formatter["train"].process(data, config, "train")
 
         def valid_collate_fn(data):
-            return formatter["valid"].process(data, config, task)
+            return formatter["valid"].process(data, config, "valid")
 
         def test_collate_fn(data):
-            return formatter["test"].process(data, config, task)
+            return formatter["test"].process(data, config, "test")
 
         if task == "train":
             collate_fn[task] = train_collate_fn
